d69c23b2079225d91bfd49ff939540557fb2432d
shashank <chitnis.shashank@gmail.com>
2019-04-23 19:15:12 -0400

    Update op2CsvDataMI.py
    plotting graphs

--- op2CsvDataMI.py
+++ op2CsvDataMI.py
@@ -1,10 +1,24 @@
 import pprint
 import pandas as pd
 import numpy as np
-import op2Util as op2u
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/skedData.csv')
 # print(df.head())
 # df = df.groupby('SkedId').mean()
-group = df['Effort'].groupby([df['SkedId'],df['Day']])
-print(group.mean())
+groupDay = df['Effort'].groupby(df['Day'])
+day_effort_mean = groupDay.mean() 
+print(day_effort_mean)
+
+day_effort_mean.plot(kind='bar', x='Day', y='effort', color='red')
+plt.plot(range(100))
+plt.show()
+
+groupTask = df['Effort'].groupby(df['TaskId'])
+task_effort_mean = groupTask.mean() 
+print(task_effort_mean)
+
+task_effort_mean.plot(kind='bar', x='TaskId', y='effort', color='red')
+plt.plot(range(100))
+plt.show()
+
