9b3312c755b88bde5d9356ffcbe5a7113fdebe9a
cshashank <chitnis.shashank@gmail.com>
2019-06-06 22:08:22 -0400

    Update skc-sci1.py

--- scikit-learn/skc-sci1.py
+++ scikit-learn/skc-sci1.py
@@ -6,15 +6,27 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 
 iris = datasets.load_iris()
-x_iris,y_iris = iris.data,iris.target
-print (x_iris.shape,y_iris.shape)
-print (x_iris[0],y_iris[0])
+X_iris,y_iris = iris.data,iris.target
+print (X_iris.shape,y_iris.shape)
+print (X_iris[0],y_iris[0])
 print (iris.target_names)
-# Get dataset with only the first two attributes
-x,y = x_iris[:,:2],y_iris
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=33)
-print (x_train.shape,y_train.shape)
+# Get dataset with only the first two attributesclear
+x,y = X_iris[:,:2],y_iris
+X_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=33)
+print (X_train.shape,y_train.shape)
 #Standardize the features
-scaler = preprocessing.StandardScaler().fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+x_test = scaler.transform(x_test)   
+print(X_train,y_train)
+
+colors = ['red','greenyellow','blue']
+for i in range(len(colors)):
+    xs = X_train[:,0][y_train == i]
+    ys = X_train[:,1][y_train == i]
+    # print(xs,ys,i)
+    plt.scatter(xs,ys,c=colors[i])
+plt.legend(iris.target_names)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.show()
