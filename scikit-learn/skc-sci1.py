6658a0eba00268bdded90021caaf30c788a247ab
cshashank <chitnis.shashank@gmail.com>
2019-06-10 01:14:10 -0400

    Update skc-sci1.py

--- scikit-learn/skc-sci1.py
+++ scikit-learn/skc-sci1.py
@@ -4,22 +4,21 @@ import sklearn as sklearn
 import numpy as numpy
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.linear_model import SGDClassifier
 
 iris = datasets.load_iris()
 X_iris,y_iris = iris.data,iris.target
-print (X_iris.shape,y_iris.shape)
-print (X_iris[0],y_iris[0])
-print (iris.target_names)
+# print (X_iris.shape,y_iris.shape)
+# print (X_iris[0],y_iris[0])
+# print (iris.target_names)
 # Get dataset with only the first two attributesclear
 x,y = X_iris[:,:2],y_iris
-X_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=33)
-print (X_train.shape,y_train.shape)
+X_train,X_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=33)
+# print (X_train.shape,y_train.shape)
 #Standardize the features
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
-x_test = scaler.transform(x_test)   
-print(X_train,y_train)
-
+X_test = scaler.transform(X_test)
 colors = ['red','greenyellow','blue']
 for i in range(len(colors)):
     xs = X_train[:,0][y_train == i]
@@ -29,4 +28,8 @@ for i in range(len(colors)):
 plt.legend(iris.target_names)
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
-plt.show()
+# plt.show()
+clf = SGDClassifier()
+clf.fit(X_train,y_train)
+print (clf.coef_)
+print(clf.intercept_)
